5e76f1e00461d09eab50c4d2a02d4551ae3b5b0e
MEvo <mvv1623@gmail.com>
2022-11-19 19:02:14 +0300

    Add sound

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.core.audio import SoundLoader
+
+
+def sound_play():
+    sound = SoundLoader.load('sources/sounds/chik.wav')
+    sound.play()
 
 class MyApp(App):
     def build(self):
-        return Button(text = "Hi W")
+        return Button(text = "Chik chik",
+                      font_size=50,
+                      on_press=self.btn_press,
+                      )
+
+
+
+    def btn_press(self, instance):
+        print("Кнопка нажата")
+        instance.text= "Я нажата"
+        sound_play()
+
+
+
 
 if __name__ == '__main__':
     MyApp().run()
